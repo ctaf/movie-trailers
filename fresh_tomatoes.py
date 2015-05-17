ee45042ac013d076c1158d7e22c1be4d2195283a
ctaf <philip.c.taferner@gmail.com>
2015-05-17 18:22:43 +0200

    Removed license header

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -1,10 +1,3 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
-__author__ = "meph"
-__date__ = "$May 14, 2015 1:31:05 PM$"
-
 import os
 import re
 import webbrowser
